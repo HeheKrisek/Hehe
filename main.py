50590e3d5d44eb366564fc8ee909a586786c9fe4
Krzychu <Szymek_p2@wp.pl>
2023-10-05 20:26:01 +0200

    Initial commit

---
+++ main.py
@@ -0,0 +1,89 @@
+import time
+import pyautogui
+
+
+
+print("hehe")
+# time.sleep(1)
+
+#H
+pyautogui.click()
+pyautogui.drag(0, 200, duration=0.25)  # move down
+pyautogui.drag(0, 200, duration=0.25)  # move down
+pyautogui.drag(0, -200, duration=0.25)  # move up
+
+pyautogui.drag(200, 0, duration=0.25)   # move right
+
+pyautogui.drag(0, 200, duration=0.25)  # move down
+pyautogui.drag(0, -200, duration=0.25)  # move up
+pyautogui.drag(0, -200, duration=0.25)  # move up
+pyautogui.drag(0, 200, duration=0.25)  # move down
+
+#hehe
+
+pyautogui.move(50, 0)
+
+#hehe
+
+#E
+pyautogui.drag(0, 200, duration=0.25)  # move down
+
+pyautogui.drag(200, 0, duration=0.25)   # move right
+pyautogui.drag(-200, 0, duration=0.25)  # move left
+
+pyautogui.drag(0, -200, duration=0.25)  # move up
+
+pyautogui.drag(200, 0, duration=0.25)   # move right
+pyautogui.drag(-200, 0, duration=0.25)  # move left
+
+pyautogui.drag(0, -200, duration=0.25)  # move up
+
+pyautogui.drag(200, 0, duration=0.25)   # move right
+pyautogui.drag(-200, 0, duration=0.25)  # move left
+
+pyautogui.drag(0, 200, duration=0.25)  # move down
+pyautogui.drag(200, 0, duration=0.25)   # move right
+
+#hehe
+
+pyautogui.move(50, 0)
+
+#hehe
+
+#H
+pyautogui.drag(0, 200, duration=0.25)  # move down
+pyautogui.drag(0, -200, duration=0.25)  # move up
+pyautogui.drag(0, -200, duration=0.25)  # move up
+pyautogui.drag(0, 200, duration=0.25)  # move down
+
+pyautogui.drag(200, 0, duration=0.25)   # move right
+
+pyautogui.drag(0, 200, duration=0.25)  # move down
+pyautogui.drag(0, -200, duration=0.25)  # move up
+pyautogui.drag(0, -200, duration=0.25)  # move up
+pyautogui.drag(0, 200, duration=0.25)  # move down
+
+#hehe
+
+pyautogui.move(50, 0)
+
+#hehe
+
+#E
+pyautogui.drag(0, 200, duration=0.25)  # move down
+
+pyautogui.drag(200, 0, duration=0.25)   # move right
+pyautogui.drag(-200, 0, duration=0.25)  # move left
+
+pyautogui.drag(0, -200, duration=0.25)  # move up
+
+pyautogui.drag(200, 0, duration=0.25)   # move right
+pyautogui.drag(-200, 0, duration=0.25)  # move left
+
+pyautogui.drag(0, -200, duration=0.25)  # move up
+
+pyautogui.drag(200, 0, duration=0.25)   # move right
+pyautogui.drag(-200, 0, duration=0.25)  # move left
+
+pyautogui.drag(0, 200, duration=0.25)  # move down
+pyautogui.drag(200, 0, duration=0.25)   # move right
